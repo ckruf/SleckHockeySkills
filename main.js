b59f69f75faa937e22367007d648ef543cdc1344
ckruf <christian.kruf@gmail.com>
2023-10-14 14:18:09 +0200

    progress

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
-function showLinks() {
-    const navToggleBtn = document.getElementById("nav-toggle");
+function toggleNavLinks() {
     const navLinks = document.getElementById("nav-links");
+    const currentHeight = window.getComputedStyle(navLinks).height;
+    console.log("currentHeight", currentHeight);
+    if (currentHeight === "0px") {
+        openNavLinks(navLinks);
+    }
+    else {
+        closeNavLinks(navLinks);
+    }
+}
 
-    navToggleBtn.addEventListener("click", () => {
-        navLinks.classList.toggle("show-links");
-    });
+function openNavLinks(navLinks) {
+    const linkCount = navLinks.childElementCount;
+    const linkHeight = navLinks.firstElementChild.offsetHeight;
+    const newHeight = linkCount * linkHeight;
+    navLinks.style.height = `${newHeight}px`;
+
+}
+
+function closeNavLinks(navLinks) {
+    navLinks.style.height = "0px";
+}
+
+const addNavToggle = () => {
+    const navToggleBtn = document.getElementById("nav-toggle");
+    navToggleBtn.addEventListener("click", toggleNavLinks);
 }
 
 
-showLinks();
+
+addNavToggle();
 
